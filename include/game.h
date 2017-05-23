d5f5509ea2cf3da82fe091b4df7beb8d7452c606
Jesus Barrera <jesus.ba015@gmail.com>
2017-05-23 07:05:01 -0500

    Add game.h with game globals

---
+++ include/game.h
@@ -0,0 +1,14 @@
+#ifndef _GAME_H_INCLUDED_
+#define _GAME_H_INCLUDED_
+
+#include <SDL.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+extern SDL_Rect info_viewport;
+extern SDL_Rect game_viewport;
+
+void set_viewports();
+
+#endif
---
+++ src/game.c
@@ -0,0 +1,17 @@
+#include "game.h"
+
+SDL_Rect info_viewport;
+SDL_Rect game_viewport;
+
+void set_viewports()
+{
+    info_viewport.x = 0;
+    info_viewport.y = 0;
+    info_viewport.w = WINDOW_WIDTH;
+    info_viewport.h = WINDOW_HEIGHT * 0.15;
+
+    game_viewport.x = 0;
+    game_viewport.y = info_viewport.h;
+    game_viewport.w = WINDOW_WIDTH;
+    game_viewport.h = WINDOW_HEIGHT - game_viewport.y;
+}
