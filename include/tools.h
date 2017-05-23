9be3c73f780a7e5796a76b9260f1d21849e3c238
Jesus Barrera <jesus.ba015@gmail.com>
2017-05-23 08:10:44 -0500

    Add draw_polygon function
    This will be useful for drawing the spaceship and the asteroids.

--- include/tools.h
+++ include/tools.h
@@ -1,6 +1,8 @@
 #ifndef _TOOLS_H_INCLUDED_
 #define _TOOLS_H_INCLUDED_
 
+#include <SDL.h>
+
 #define PI 3.14159265358979323846
 
 typedef struct Point Point;
@@ -11,5 +13,6 @@ struct Point {
 };
 
 float uniform(float min, float max);
+void draw_polygon(SDL_Renderer *renderer, Point *points, int points_count);
 
 #endif
